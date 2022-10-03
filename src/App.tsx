8bb14f69ae98517f5ac7c02bf5e5a2cd51612a90
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-10-03 13:52:44 +0200

    Fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <div hidden>
         <label>
-            How much to you make?<br/>
+            How much do you make?<br/>
           <input
             type={"number"}
               value={p1}
