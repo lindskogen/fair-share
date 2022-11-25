34c0b2455b24eb76224efba6013f77659ad439f0
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-11-25 18:04:39 +0100

    Persist entered values in localStorage
    - Add private mode to hide the actual numbers, also persisted
    - Responsive tweaks and CSS cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,72 @@
 import { useState } from "react";
 import "./App.css";
+import { useLocalStorageState } from "./hooks/useLocalStorageState";
 
-const formatter = new Intl.NumberFormat(undefined, { currency: "SEK", style: "currency" });
+const formatter = new Intl.NumberFormat(undefined, {
+  currency: "SEK",
+  style: "currency",
+});
 
 function App() {
-  const [p1, setP1] = useState(1);
-  const [p2, setP2] = useState(1);
+  const [hidden, setHidden] = useLocalStorageState("hidden", false);
+  const [p1, setP1] = useLocalStorageState("p1", 1);
+  const [p2, setP2] = useLocalStorageState("p2", 1);
   const [cost, setCost] = useState(100);
 
   return (
     <div className="App">
-      <div>
+      <div className="settings">
         <label>
-            How much do you make?<br/>
+          <input
+            type="checkbox"
+            checked={hidden}
+            onChange={() => setHidden((h) => !h)}
+          />
+          {" Hide sensitive data"}
+        </label>
+      </div>
+      <div hidden={hidden}>
+        <label>
+          How much do you make?
+          <br />
           <input
             type={"number"}
-              value={p1}
+            value={p1}
             onChange={(e) => setP1(e.currentTarget.valueAsNumber)}
           />
         </label>
       </div>
 
-        <div style={{ padding: 10 }}></div>
-
-      <div>
+      <div hidden={hidden}>
         <label>
-          How much does your partner make?<br/>
+          How much does your partner make?
+          <br />
           <input
             type={"number"}
-              value={p2}
+            value={p2}
             onChange={(e) => setP2(e.currentTarget.valueAsNumber)}
           />
         </label>
       </div>
 
-        <div style={{ padding: 10 }}></div>
+      <div>
+        <label>
+          What is the cost of the thing?
+          <br />
+          <input
+            type={"number"}
+            value={cost}
+            onChange={(e) => setCost(e.currentTarget.valueAsNumber)}
+          />
+        </label>
+      </div>
 
+      <div>
+        <div>You will pay: {formatter.format((p1 / (p1 + p2)) * cost)}</div>
         <div>
-            <label>
-                What is the cost of the thing?<br/>
-                <input
-                    type={"number"}
-                    value={cost}
-                    onChange={(e) => setCost(e.currentTarget.valueAsNumber)}
-                />
-            </label>
+          Your partner will pay: {formatter.format((p2 / (p1 + p2)) * cost)}
         </div>
-
-        <div style={{ padding: 10 }}></div>
-
-    <div>You will pay: {formatter.format(p1/(p1 + p2) * cost)}</div>
-        <div>Your partner will pay: {formatter.format(p2/(p1 + p2) * cost)}</div>
+      </div>
     </div>
   );
 }
