e14bdbbf7c46ed9701c4880bc5d265436f2a261b
Johan Lindskogen <johan.lindskogen@gmail.com>
2023-02-26 21:58:00 +0100

    Add type definitions

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,8 +1,8 @@
-export function getLocalStorage(name, value) {
+export function getLocalStorage<T>(name: string, defaultValue: T): T {
   let v = window.localStorage.getItem(name);
-  return v == null ? value : JSON.parse(v);
+  return v == null ? defaultValue : JSON.parse(v);
 }
 
-export function setLocalStorage(name, value) {
+export function setLocalStorage(name: string, value: unknown) {
   window.localStorage.setItem(name, JSON.stringify(value));
 }
